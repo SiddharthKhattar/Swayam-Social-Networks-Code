569d02168b90c8980f85532166b08800d740eab1
SiddharthKhattar <skhattar111@gmail.com>
2022-10-18 22:37:38 +0530

    Coding Cascade Model V1

--- Week 12/Coding Cascade Model.py
+++ Week 12/Coding Cascade Model.py
@@ -2,6 +2,9 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import random  
+import numpy as np
+
+
 
 def ic(G,s):
     print(s)
@@ -43,8 +46,8 @@ def find_nodes(h, it):
   
   
 # Create graph object and add nodes
-g = nx.Graph()
-g.add_edges_from(
+G = nx.Graph()
+G.add_edges_from(
     [(1, 2), (1, 9), (3, 13), (4, 6),
      (5, 6), (5, 7), (5, 8), (5, 9), 
      (5, 10), (5, 11), (5, 12), (10, 12), 
@@ -54,4 +57,45 @@ g.add_edges_from(
   
 seed = [3,8]
 
-ic(g,seed)
+dict_deg = {}
+dict_cl = {}
+dict_bw = {}
+dict_cr = {}
+
+for each in G.nodes():
+    dict_deg[each] = G.degree(each)
+    dict_cl[each] = nx.closeness_centrality(G,each)
+    dict_bw[each] = nx.betweenness_centrality(G,each)
+    dict_cr[each] = nx.core_number(G)[each]
+
+dict_cascade= {}
+
+for each in G.nodes():
+    c = []
+    for num in range(0,1000):
+        seed = [each]
+        i = ic(G,seed)
+        c.append(len(i))
+    dict_cascade[each] = np.average(c)    
+
+sorted_dict_cascade = sorted(dict_cascade,key=dict_cascade.get,reverse=True)
+sorted_dict_deg = sorted(dict_deg,key=dict_deg.get,reverse=True)
+sorted_dict_cl = sorted(dict_cl,key=dict_cl.get,reverse=True)
+sorted_dict_bw = sorted(dict_bw,key=dict_bw.get,reverse=True)
+sorted_dict_cr = sorted(dict_cr,key=dict_cr.get,reverse=True)
+print("Nodes sorted according to degree")
+print(sorted_dict_deg)
+print("Nodes sorted according to closeness")
+
+print(sorted_dict_cl)
+print("Nodes sorted according to betweeness")
+
+print(sorted_dict_bw)
+print("Nodes sorted according to cr")
+
+print(sorted_dict_cr)
+print("Nodes sorted according to cascade")
+
+print(sorted_dict_cascade)
+
+# ic(G,seed)
